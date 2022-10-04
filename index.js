943573ebe159b90c5ed54e4649160ea70b89f59f
om <oshewale@asu.edu>
2022-10-04 14:06:32 -0700

    audio

--- index.js
+++ index.js
@@ -2,9 +2,36 @@ var ndrums=document.querySelectorAll(".drum").length;
 
 for (var i=0;i<ndrums;i++){
   document.querySelectorAll(".drum")[i].addEventListener("click",function() {
-    var j=Math.ceil(Math.random()*6);
-    var audnum="sounds/tom-"+j+".mp3";
-  var audio = new Audio(audnum);
-  audio.play();
+  switch(this.innerHTML){
+    case "w":
+    var audio = new Audio('sounds/tom-1.mp3');
+    audio.play();
+    break;
+    case "a":
+    var audio = new Audio('sounds/tom-2.mp3');
+    audio.play();
+    break;
+    case "s":
+    var audio = new Audio('sounds/tom-3.mp3');
+    audio.play();
+    break;
+    case "d":
+    var audio = new Audio('sounds/tom-4.mp3');
+    audio.play();
+    break;
+    case "j":
+    var audio = new Audio('sounds/tom-5.mp3');
+    audio.play();
+    break;
+    case "k":
+    var audio = new Audio('sounds/tom-6.mp3');
+    audio.play();
+    break;
+    case "l":
+    var audio = new Audio('sounds/crash.mp3');
+    audio.play();
+    break;
+  }
+
 });
 }
